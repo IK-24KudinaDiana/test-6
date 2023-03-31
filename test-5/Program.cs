201ec83e8af70029f6e92daf6d4a6881e03e068d
IK-24KudinaDiana <kudinadiana91@gmail.com>
2023-03-31 14:32:42 +0300

    add test-5

---
+++ test-5/Program.cs
@@ -0,0 +1,90 @@
+﻿using test_5;
+Random rand = new Random();
+double x = rand.NextDouble(-100,100) ;
+double y = rand.NextDouble() ;
+//double x1, y1, x2, y2, x3, y3;
+//Console.WriteLine("Enter the coordinates of the triangle vertices:");
+//Console.Write("x1 = ");
+//x1 = double.Parse(Console.ReadLine());
+//Console.Write("y1 = ");
+//y1 = double.Parse(Console.ReadLine());
+//Console.Write("x2 = ");
+//x2 = double.Parse(Console.ReadLine());
+//Console.Write("y2 = ");
+//y2 = double.Parse(Console.ReadLine());
+//Console.Write("x3 = ");
+//x3 = double.Parse(Console.ReadLine());
+//Console.Write("y3 = ");
+//y3 = double.Parse(Console.ReadLine());
+
+//Triangle triangle = new Triangle(x1, y1, x2, y2, x3, y3);
+
+//int choice;
+//do
+//{
+//	Console.Clear();
+//	Console.WriteLine("1. Перевірити, чи рівні два трикутники");
+//	Console.WriteLine("2. Обчислити площу трикутника");
+//	Console.WriteLine("3. Обчислити периметр трикутника");
+//	Console.WriteLine("4. Обчислити висоти трикутника");
+//	Console.WriteLine("5. Обчислити медіани трикутника");
+//	Console.WriteLine("6. Обчислити бісектриси трикутника");
+//	Console.WriteLine("7. Обчислити радіус вписаного кола трикутника");
+//	Console.WriteLine("8. Обчислити радіус описаного кола трикутника");
+//	Console.WriteLine("9. Визначити тип трикутника");
+//	Console.WriteLine("10. Повернути трикутник на заданий кут");
+//	Console.WriteLine("0. Вихід");
+//	Console.Write("Виберіть опцію: ");
+//	choice = int.Parse(Console.ReadLine());
+
+//	switch (choice)
+//	{
+//		case 1:
+//			Console.WriteLine("Обраний пункт 1");
+//			// код для порівняння двох трикутників
+//			break;
+//		case 2:
+//			Console.WriteLine("Обраний пункт 2");
+//			// код для обчислення площі трикутника
+//			break;
+//		case 3:
+//			Console.WriteLine("Обраний пункт 3");
+//			// код для обчислення периметра трикутника
+//			break;
+//		case 4:
+//			Console.WriteLine("Обраний пункт 4");
+//			// код для обчислення висот трикутника
+//			break;
+//		case 5:
+//			Console.WriteLine("Обраний пункт 5");
+//			// код для обчислення медіан трикутника
+//			break;
+//		case 6:
+//			Console.WriteLine("Обраний пункт 6");
+//			// код для обчислення бісектрис трикутника
+//			break;
+//		case 7:
+//			Console.WriteLine("Обраний пункт 7");
+//			// код для обчислення радіуса вписаного кола трикутника
+//			break;
+//		case 8:
+//			Console.WriteLine("Обраний пункт 8");
+//			// код для обчислення радіуса описаного кола трикутника
+//			break;
+//		case 9:
+//			Console.WriteLine("Обраний пункт 9");
+//			// код для визначення типу трикутника
+//			break;
+//		case 10:
+//			Console.WriteLine("Обраний пункт 10");
+//			// код для повороту трикутника
+//			break;
+//		case 0:
+//			Console.WriteLine("Goodbye!");
+//			break;
+//		default:
+//			Console.WriteLine("Виберіть правильну опцію");
+//			break;
+//	}
+//	Console.WriteLine();
+//} while (choice != 2);
---
+++ test-5/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_5
+{
+	internal class Triangle
+	{
+		private double x1, y1, x2, y2, x3, y3;
+
+		public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+		{
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+			this.x3 = x3;
+			this.y3 = y3;
+		}
+
+		public double Area()
+		{
+			double side1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+			double side2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+			double side3 = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+			double s = (side1 + side2 + side3) / 2;
+			return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+		}
+	}
+}
